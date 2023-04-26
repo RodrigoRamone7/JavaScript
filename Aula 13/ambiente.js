a16e4e80e8a264af646555d8575e59e0ef5c32d4
RodrigoRamone7 <69323304+RodrigoRamone7@users.noreply.github.com>
2023-04-26 14:13:51 -0300

    aprendendo loops
    Próximos passos serão resolver os exercícios

--- Aula 13/ambiente.js
+++ Aula 13/ambiente.js
@@ -1,5 +1,27 @@
+/*
+ESTRUTURA DE CÓDIGO
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+/*
+ESTRUTURA WHILE
+var c = 1
+while (c<=3){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+ESTRUTURA DO WHILE
 var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c<=6)
+*/
---
+++ Aula 14/ambiente.js
@@ -0,0 +1,7 @@
+console.log('Vai comecar...')
+
+for(c=1;c<=10;c++){
+    console.log(`Passo ${c}`)
+}
+
+console.log('FIM!')
